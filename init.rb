85247dbedd37a9cd42be6b2268656bbd4ff8f4ef
tkusukawa <t@kusukawa.jp>
2016-10-03 02:28:07 +0900

    Fixed probrem that blocked user can refer the description of issue by query, csv, pdf.

--- init.rb
+++ init.rb
@@ -1,10 +1,14 @@
 require 'redmine'
 
 require_dependency 'block_issue_content_issue_patch'
+require_dependency 'block_issue_content_query_patch'
 
 Rails.configuration.to_prepare do
   unless Issue.included_modules.include?(BlockIssueContentIssuePatch)
     Issue.send(:include, BlockIssueContentIssuePatch)
+  end
+  unless Query.included_modules.include?(BlockIssueContentQueryPatch)
+    Query.send(:include, BlockIssueContentQueryPatch)
   end
 end
 
@@ -12,7 +16,7 @@ Redmine::Plugin.register :block_issue_content do
   name 'Block Issue Content plugin'
   author 'Tomohisa Kusukawa'
   description 'Redmine plugin to block the view of issue content'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/tkusukawa/block_issue_content'
   author_url 'https://github.com/tkusukawa'
 
